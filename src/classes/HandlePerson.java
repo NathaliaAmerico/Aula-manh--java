0c6ce71343823aed05a78ca9e45a5ad205c8eb1a
Nathália Américo <Namericonascimento@gmail.com>
2022-07-13 14:45:51 -0300

    Aula manhã java

---
+++ src/classes/HandlePerson.java
@@ -0,0 +1,13 @@
+package classes;
+
+public class HandlePerson {
+
+    static Person person = new Person("Nathalia", 32, "Dev");
+
+   public static void main(String[] args) {
+
+     System.out.println(person.getName());
+   
+    }
+
+  }
